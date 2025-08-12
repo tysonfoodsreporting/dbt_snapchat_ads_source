af29fe5b76467aa1253d4e8abe0019774aeb100a
Sandeep Nabar <Sandeepnab@mediabymother.com>
2025-08-12 19:46:03 +0530

    Fixes for Timestamp

--- models/stg_snapchat_ads__ad_account_history.sql
+++ models/stg_snapchat_ads__ad_account_history.sql
@@ -31,14 +31,14 @@ final as (
         id as ad_account_id,
         name as ad_account_name,
         --cast (created_at as {{ dbt.type_timestamp() }}) as created_at,
-        TIMESTAMP(CAST(created_at AS {{ dbt.type_timestamp() }}), "America/Chicago") as created_at,
+        TIMESTAMP(DATETIME(CAST(created_at AS {{ dbt.type_timestamp() }}), "America/Chicago")) as created_at,
         advertiser, 
         currency,
         timezone,
         --cast (_fivetran_synced as {{ dbt.type_timestamp() }}) as _fivetran_synced,
-        TIMESTAMP(CAST(_fivetran_synced AS {{ dbt.type_timestamp() }}), "America/Chicago") as _fivetran_synced,
+        TIMESTAMP(DATETIME(CAST(_fivetran_synced AS {{ dbt.type_timestamp() }}), "America/Chicago")) as _fivetran_synced,
         --cast (updated_at as {{ dbt.type_timestamp() }}) as updated_at,
-         TIMESTAMP(CAST(updated_at AS {{ dbt.type_timestamp() }}), "America/Chicago") as updated_at,
+        TIMESTAMP(DATETIME(CAST(updated_at AS {{ dbt.type_timestamp() }}), "America/Chicago")) as updated_at,
         row_number() over (partition by source_relation, id order by _fivetran_synced desc) = 1 as is_most_recent_record
     from fields
 )
